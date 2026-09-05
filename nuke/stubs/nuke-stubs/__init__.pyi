f30cb69f245205ebbe5b8696dbb1662e6171a9a7
Nathan Rusch <nrusch@github>
2026-09-04 19:08:05 -0700

    Apply manual __init__ updates

--- nuke/stubs/nuke-stubs/__init__.pyi
+++ nuke/stubs/nuke-stubs/__init__.pyi
@@ -6,4 +6,12 @@ from .overrides import *
 from .scripts import scriptSaveAndClear as scriptSaveAndClear
 from .utils import *
 
-def import_module(name, filterRule) -> None: ...
+from . import geo as geo
+from . import localization as localization
+from . import memory2 as memory2
+from . import rotopaint as rotopaint
+from . import splinewarp as splinewarp
+from . import _curvelib as curvelib
+from . import _curveknob as curveknob
+from . import _nukemath as nukemath
+from . import _gsv as gsv
